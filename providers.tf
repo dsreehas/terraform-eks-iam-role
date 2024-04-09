18f3f9bc68432cf4fae5474cba1ed6d5cd37091a
dsreehas <dsreehas@gmail.com>
2024-04-09 17:59:42 -0400

    modified access_key, secret_key variable names

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
     region     = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
