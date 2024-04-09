9aa94089c258bff4230cee6cf03bec1797fbced1
dsreehas <dsreehas@gmail.com>
2024-04-09 17:31:19 -0400

    added .gitignore

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "AWS account access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS account secret access key"
+  type        = string
+}
