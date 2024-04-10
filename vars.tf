cc8d88dfabe301227f046b5233d484f531d32f49
dsreehas <dsreehas@gmail.com>
2024-04-09 23:36:11 -0400

    removed terraform fmt from github actions workflow

--- vars.tf
+++ vars.tf
@@ -3,16 +3,6 @@ variable "aws_region" {
   type = string
 }
 
-# variable "aws_access_key" {
-#   description = "AWS access key"
-#   type = string
-# }
-
-# variable "aws_secret_key" {
-#   description = "AWS secret key"
-#   type = string
-# }
-
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
