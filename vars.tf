e518aab19d9737e46e74fcabf983c2bf33e54c6d
dsreehas <dsreehas@gmail.com>
2024-04-09 23:10:34 -0400

    fixed vars.tf warnings from tflint

--- vars.tf
+++ vars.tf
@@ -3,17 +3,18 @@ variable "aws_region" {
   type = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type = string
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type = string
+# }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
+  default     = "demo-cluster"
 }
