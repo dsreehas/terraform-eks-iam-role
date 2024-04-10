4847197cc139c049cbf5946d650151825dfd74f9
dsreehas <dsreehas@gmail.com>
2024-04-09 21:06:43 -0400

    removed unused variables

--- modules/eks-node-group-role/vars.tf
+++ modules/eks-node-group-role/vars.tf
@@ -3,11 +3,6 @@ variable "cluster_name" {
   type        = string
 }
 
-# variable "ec2_service_principal" {
-#   description = "Service principal for EKS"
-#   default     = "eks.amazonaws.com"
-# }
-
 variable "worker_node_policy_arn" {
   description = "ARN of the IAM policy to attach to worker nodes"
   default     = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
